78031e7c6b9b8fcd9b9cc891abda0e3d3d676a46
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-07-17 14:50:34 +0200

    fix anime2
    Signed-off-by: DESCARAVAGE <escaravage.daniel@gmail.com>

--- app/ui/components/slides/index.tsx
+++ app/ui/components/slides/index.tsx
@@ -7,11 +7,21 @@ export default function Slides() {
     return (
         <div>
             <section className="slide1">
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, ease: "easeInOut" }} className='flex flex-col'>
-                    <h4 className={`${cinzel.className} max-w-xs  font-semibold leading-10 tracking-tight text center text-black dark:text-black-50`}>
+                <motion.div 
+                  className='flex flex-col'
+                  initial={{ opacity: 0, y: -40 }} 
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ type: "spring"}}>
+                    <h4 className={`${cinzel.className}
+                       max-w-xs font-semibold leading-10 tracking-tight text centertext-black dark:text-black-50`}>
                         Aesteria - Photographe
                     </h4>
-                    <motion.button whileHover={{ scale: 1.05, y: -2 }} whileTap={{ scale: 0.85, y: 1 }} className='flex flex-grid'>
+                    <motion.button 
+                      className='flex flex-grid'
+                      whileHover={{ scale: 1.05, y: -2 }} 
+                      whileTap={{ scale: 0.85, y: 1 }}
+                      transition={{ type: "spring"}}
+                    >
                         <a
                             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
                             href="https://www.instagram.com/aesteria.photo/"
