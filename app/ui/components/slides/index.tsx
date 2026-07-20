96cfc8ce3a7e8baf6b06439b9fda2b3fe9e1486c
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-07-20 14:07:20 +0200

    Add column
    Signed-off-by: DESCARAVAGE <escaravage.daniel@gmail.com>

--- app/ui/components/slides/index.tsx
+++ app/ui/components/slides/index.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { cinzel } from '../../fonts'
 import { FaInstagram } from 'react-icons/fa'
 import { FaLinkedin } from "react-icons/fa";
-
+import { Box, Grid } from '@mui/material'
 import ImageSlider from './imageSlider'
 
 export interface PropsType {
@@ -34,11 +34,20 @@ export default function Slides() {
         margin: "0 auto",
     }
 
+
+    const items = [1, 2, 3, 4]
+
+
     return (
-        <div>
-            <section className='slide1 z-1'>
+        // <section className='slide1 z-1'>
+
+        // </section>
+        <Grid container spacing={2} sx={{ marginInline: '15rem' }}>
+            <Grid size={{ xs: 12, md: 6 }} sx={{
+                display: "flex", justifyContent: 'center', alignItems: 'center'
+            }}>
                 <a
-                    className="flex h-12 w-fit items-center gap-2 px-5 text-background transition-colors"
+                    className="flex w-fit text-background"
                     href={`https://www.instagram.com/${insta}/`}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -53,8 +62,11 @@ export default function Slides() {
                         <p>Instagram</p>
                     </motion.button>
                 </a>
+
+            </Grid>
+            <Grid size={{ xs: 12, md: 6 }} sx={{ display: "flex", justifyContent: 'center' }}>
                 <a
-                    className="flex h-12 w-fit items-center gap-2 px-5 text-background transition-colors"
+                    className="flex w-fit items-center text-background"
                     href={`https://www.linkedin.com/in/${linkedin}/`}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -69,49 +81,83 @@ export default function Slides() {
                         <p>Linkdin</p>
                     </motion.button>
                 </a>
-            </section>
-            {/* <section className="slide1 z-1">
-
-                <motion.div
-                    className='flex flex-col w-fit justify-items-center'
-                    initial={{ opacity: 0, y: -40 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ type: "spring", stiffness: 100 }}>
-                    <div className='justify-items-center'>
-                        <h4 className={`${cinzel.className}
-                       max-w-xs font-semibold leading-10 tracking-tight text centertext-black dark:text-zinc-500`}>
-                            Aesteria - Photographe
-                        </h4>
-                        <div style={containerStyles}>
-                            <ImageSlider slides={slides} />
-                        </div>
-                    </div>
-                
-
-                </motion.div>
-
-            </section> */}
-            {/* <section className='backG flex flex-col'>
-                <motion.div className="text-center  h-screen">
-                    <p>À bientôt !</p>
-                </motion.div>
-            </section> */}
-
-            {/* <section className="scroll-h">
-        <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 100}} transition={{ duration: 0.8 }} className="">
-          peut-être
-        </motion.div>
-      </section>
-      <section className="slide3">
-        <motion.div className="">
-          non
-        </motion.div>
-      </section><section className="slide4">
-        <motion.div className="">
-          oui
-        </motion.div>
-      </section>
-       */}
-        </div>
+            </Grid>
+        </Grid>
     )
 }
+
+
+// <a
+//                     className="flex w-fit items-center text-background"
+//                     href={`https://www.instagram.com/${insta}/`}
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     <motion.button
+//                         whileHover={{ scale: 1.05, y: -1 }}
+//                         whileTap={{ scale: 0.85, y: 1 }}
+//                         transition={{ type: "spring" }}
+//                         className='btn-link justify-items-center'
+//                     >
+//                         <FaInstagram />
+//                         <p>Instagram</p>
+//                     </motion.button>
+//                 </a>
+//                 <a
+//                     className="flex h-12 w-fit items-center text-background"
+//                     href={`https://www.linkedin.com/in/${linkedin}/`}
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     <motion.button
+//                         whileHover={{ scale: 1.05, y: -1 }}
+//                         whileTap={{ scale: 0.85, y: 1 }}
+//                         transition={{ type: "spring" }}
+//                         className='btn-link justify-items-center'
+//                     >
+//                         <FaLinkedin />
+//                         <p>Linkdin</p>
+//                     </motion.button>
+//                 </a>    
+{/* <section className="slide1 z-1">
+
+    <motion.div
+        className='flex flex-col w-fit justify-items-center'
+        initial={{ opacity: 0, y: -40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 100 }}>
+        <div className='justify-items-center'>
+            <h4 className={`${cinzel.className}
+           max-w-xs font-semibold leading-10 tracking-tight text centertext-black dark:text-zinc-500`}>
+                Aesteria - Photographe
+            </h4>
+            <div style={containerStyles}>
+                <ImageSlider slides={slides} />
+            </div>
+        </div>
+    
+
+    </motion.div>
+
+</section> */}
+{/* <section className='backG flex flex-col'>
+    <motion.div className="text-center  h-screen">
+        <p>À bientôt !</p>
+    </motion.div>
+</section> */}
+
+{/* <section className="scroll-h">
+<motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 100}} transition={{ duration: 0.8 }} className="">
+peut-être
+</motion.div>
+</section>
+<section className="slide3">
+<motion.div className="">
+non
+</motion.div>
+</section><section className="slide4">
+<motion.div className="">
+oui
+</motion.div>
+</section>
+*/}
