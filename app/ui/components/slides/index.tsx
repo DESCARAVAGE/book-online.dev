1c1608c2d3b5ef73f2c021c82a452797296b6577
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-08-19 13:04:16 +0200

    Ornement et dividers
    Signed-off-by: DESCARAVAGE <escaravage.daniel@gmail.com>

--- app/ui/components/slides/index.tsx
+++ app/ui/components/slides/index.tsx
@@ -3,6 +3,8 @@ import GalleryCards from "./galerie";
 import CollectionsScroll from "./collections";
 import AboutMe from "./aboutMe";
 import LandingSection from "./landingSection";
+import Approch from "./approch";
+import Gal from "./galerie/index2";
 
 export default function Slides() {
 
@@ -12,11 +14,17 @@ export default function Slides() {
         <LandingSection />
         {/* <GalleryCards /> */}
       </section>
+      <section id="theme">
+        <Approch />
+      </section>
       <section id="collections">
         {/* <CollectionsScroll /> */}
       </section>
       <section id="a-propos">
         <AboutMe />
+      </section>
+      <section id="gallery">
+       <Gal />
       </section>
     </>
   );
